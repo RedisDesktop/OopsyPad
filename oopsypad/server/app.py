a561d0c4a9d639e06e4e9fc16b34f477f5f4cf55
ommrnom <ommarinaom@gmail.com>
2017-10-24 11:53:17 +0300

    Remove unnecessary checks

--- oopsypad/server/app.py
+++ oopsypad/server/app.py
@@ -34,40 +34,34 @@ def process_minidump(minidump_id):
 #
 # Send Crash Report
 # POST /crash-report
-@app.route('/crash-report', methods=['GET', 'POST'])
+@app.route('/crash-report', methods=['POST'])
 def add_minidump():
-    if request.method == 'POST':
-        # NOTE (COMMAND EXAMPLE TO CHECK):
-        # curl <site_host>/crash-report -F upload_file_minidump=@</path/to/dump_file>
-        # -F product=<product> -F version=<version> -F platform=<platform>
-        data = request.form
-        version = data['version']
-        if version < "0.8":
-            return jsonify({
-                "error": "You use an old version. "
-                         "Please download the latest release <a href=\"http://redisdesktop.com/download\">0.8.0</a>"}
-            ), 400
-        try:
-            minidump = models.Minidump.create_minidump(request)
-            process_minidump.delay(str(minidump.id))
-        except Exception as e:
-            return jsonify({"error": "Something went wrong: {}".format(e)}), 400
+    # NOTE (COMMAND EXAMPLE TO CHECK):
+    # curl <site_host>/crash-report -F upload_file_minidump=@</path/to/dump_file>
+    # -F product=<product> -F version=<version> -F platform=<platform>
+    data = request.form
+    version = data['version']
+    if version < "0.8":
+        return jsonify({
+            "error": "You use an old version. "
+                     "Please download the latest release <a href=\"http://redisdesktop.com/download\">0.8.0</a>"}
+        ), 400
+    try:
+        minidump = models.Minidump.create_minidump(request)
+        process_minidump.delay(str(minidump.id))
+    except Exception as e:
+        return jsonify({"error": "Something went wrong: {}".format(e)}), 400
 
-        return jsonify({"ok": "Thank you!"}), 201
-    else:
-        return jsonify({"ok": "Try POST"})
+    return jsonify({"ok": "Thank you!"}), 201
 
 
-@app.route('/data/symfiles/<product>/<id>', methods=['GET', 'POST'])
+@app.route('/data/symfiles/<product>/<id>', methods=['POST'])
 def add_symfile(product, id):
-    if request.method == 'POST':
-        try:
-            models.SymFile.create_symfile(request, product, id)
-            return jsonify({"ok": "Symbol file saved."}), 201
-        except Exception as e:
-            return jsonify({"error": "Something went wrong: {}".format(e)}), 400
-    else:
-        return jsonify({"ok": "Try POST"})
+    try:
+        models.SymFile.create_symfile(request, product, id)
+        return jsonify({"ok": "Symbol file saved."}), 201
+    except Exception as e:
+        return jsonify({"error": "Something went wrong: {}".format(e)}), 400
 
 
 if __name__ == '__main__':
