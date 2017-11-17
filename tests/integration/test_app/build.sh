660b0de6507489f4b298694f2c69822f6587a6ee
Igor Malinovskiy <u.glide@gmail.com>
2017-11-17 14:30:55 +0200

    Update build.sh

--- tests/integration/test_app/build.sh
+++ tests/integration/test_app/build.sh
@@ -9,5 +9,5 @@ cd CrashReporter
 qmake "DESTDIR=./../bin/" \
       'DEFINES+=APP_NAME=\\\"test_app\\\"' \
       'DEFINES+=CRASH_SERVER_URL=\\\"http://127.0.0.1:8080/crash-report\\\"' \
-      'DEFINES+=APP_VERSION\\\"1.0.0\\\"'
+      'DEFINES+=APP_VERSION=\\\"1.0.0\\\"'
 make -s -j 2
