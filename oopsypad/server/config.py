60bef1f73271fd0ebc5570dfce27bcc7dd2720d3
ommrnom <ommarinaom@gmail.com>
2017-11-20 18:27:53 +0200

    Update config.py

--- oopsypad/server/config.py
+++ oopsypad/server/config.py
@@ -1,8 +1,44 @@
-MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB
+import os
+CWD = os.path.dirname(os.path.abspath(__file__))
+DEV, TEST, PROD = 'dev', 'test', 'prod'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+DB_NAMES = {
+    DEV: 'oopsy_pad_dev',
+    TEST: 'oopsy_pad_test',
+    PROD: 'oopsy_pad'
+}
 
-SYMFILES_DIR = 'symbols'
 
-MONGODB_SETTINGS = {'DB': "oopsy_pad"}
+class Config:
+    SECRET_KEY = os.urandom(24)
+
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB
+
+    CELERY_BROKER_URL = 'redis://localhost:6379/1'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+    DUMPS_DIR = os.path.join(CWD, 'dumps')
+    SYMFILES_DIR = os.path.join(CWD, 'symbols')
+
+
+class DevConfig(Config):
+    DEBUG = True
+    MONGODB_SETTINGS = {'DB': DB_NAMES[DEV]}
+
+
+class TestConfig(Config):
+    DEBUG = False
+    TESTING = True
+
+    MONGODB_SETTINGS = {'DB': DB_NAMES[TEST]}
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    MONGODB_SETTINGS = {'DB': DB_NAMES[PROD]}
+
+
+app_config = {
+    DEV: DevConfig,
+    TEST: TestConfig,
+    PROD: ProdConfig
+}
