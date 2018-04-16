e9b44bb0163b87db1b0c645cfaea1f28b061b939
ommrnom <ommarinaom@gmail.com>
2018-04-16 16:47:34 +0300

    Setup token auth

--- oopsypad/client/admin.py
+++ oopsypad/client/admin.py
@@ -1,14 +1,34 @@
-import click
+import configparser
 
+import click
 import requests
 
-from oopsypad.client.base import oopsy, get_address
+from oopsypad.client.base import (oopsy, get_address, get_token,
+                                  OOPSY_CONFIG_PATH)
 
 
 @oopsy.group(name='oopsy_admin')
 @click.pass_context
 def oopsy_admin(ctx):
     get_address(ctx)
+
+
+@oopsy_admin.command(name='login')
+@click.option('--user', '-u', prompt='Email')
+@click.option('--password', '-p', prompt=True, hide_input=True)
+@click.pass_context
+def login(ctx, user, password):
+    response = requests.get(
+        '{}/token'.format(ctx.obj['ADDRESS']), auth=(user, password))
+    if response.status_code == 200:
+        config = configparser.ConfigParser()
+        config['oopsypad'] = {}
+        config['oopsypad']['token'] = response.json().get('token')
+        with open(OOPSY_CONFIG_PATH, 'w') as configfile:
+            config.write(configfile)
+        click.echo('Logged in as {}'.format(user))
+    else:
+        click.echo('Unauthorized')
 
 
 @oopsy_admin.group(name='project')
@@ -20,24 +40,53 @@ def project():
 @click.argument('project-name')
 @click.option('--min-version', '-v', help='Project minimum allowed version.')
 @click.option('--platforms', '-p', multiple=True,
-              help='Allowed platforms (multiple values are acceptable, e.g. -p <p1> -p <p2>).')
+              help='Allowed platforms (multiple values are acceptable, '
+                   'e.g. -p <p1> -p <p2>).')
 @click.pass_context
 def add_project(ctx, project_name, min_version, platforms):
+    headers = {'Authorization': get_token()}
     data = {'min_version': min_version, 'allowed_platforms': platforms}
-    response = requests.post('{}/project/{}'.format(ctx.obj['ADDRESS'], project_name), data=data)
-    click.echo(response.text)
+    response = requests.post(
+        '{}/api/project/{}'.format(ctx.obj['ADDRESS'], project_name),
+        data=data, headers=headers)
+    if response.status_code == 201:
+        click.echo(response.json().get('ok', 'OK'))
+    else:
+        click.echo(response.json().get('error', 'ERROR'))
 
 
 @project.command(name='delete')
 @click.argument('project-name')
 @click.pass_context
 def delete_project(ctx, project_name):
-    response = requests.delete('{}/project/{}/delete'.format(ctx.obj['ADDRESS'], project_name))
-    click.echo(response.text)
+    headers = {'Authorization': get_token()}
+    response = requests.delete(
+        '{}/api/project/{}/delete'.format(ctx.obj['ADDRESS'], project_name),
+        headers=headers)
+    if response.status_code == 202:
+        click.echo(response.json().get('ok', 'OK'))
+    else:
+        click.echo(response.json().get('error', 'ERROR'))
 
 
 @project.command(name='list')
 @click.pass_context
 def list_projects(ctx):
-    response = requests.get('{}/project/all'.format(ctx.obj['ADDRESS']))
-    click.echo(response.text)
+    headers = {'Authorization': get_token()}
+    response = requests.get('{}/api/project/all'.format(ctx.obj['ADDRESS']),
+                            headers=headers)
+    if response.status_code == 200:
+        projects = response.json().get('projects')
+        if projects:
+            click.echo('Projects:')
+            for i, p in enumerate(projects):
+                click.echo('{} {}'.format(i + 1, p['name']))
+                click.echo(
+                    '  Minimum allowed version: {}'.format(p['min_version']))
+                click.echo(
+                    '  Allowed Platforms: {}'.format(
+                        ', '.join(p['allowed_platforms'])))
+        else:
+            click.echo('No projects.')
+    else:
+        click.echo(response.json().get('error', 'ERROR'))
--- oopsypad/client/base.py
+++ oopsypad/client/base.py
@@ -1,17 +1,35 @@
-import click
+import base64
+import configparser
 import os
+
+import click
 import requests
 import urllib3
+
+
+OOPSY_CONFIG_PATH = os.path.join(os.path.expanduser('~'), '.oopsy')
 
 
 def get_address(ctx=None):
     address = os.environ.get('OOPSY_HOST')
     if not address:
-        raise click.UsageError('OOPSY_HOST environment variable was not specified.')
+        raise click.UsageError(
+            'OOPSY_HOST environment variable was not specified.')
     if ctx:
         ctx.obj = dict()
         ctx.obj['ADDRESS'] = address
     return address
+
+
+def get_token():
+    config = configparser.ConfigParser()
+    config.read(OOPSY_CONFIG_PATH)
+    if 'oopsypad' in config:
+        if 'token' in config['oopsypad']:
+            token = config['oopsypad']['token']
+            return base64.b64encode(token.encode())
+    raise click.ClickException('Missing auth token.\n'
+                               'To login use \033[33moopsy_admin login\033[0m')
 
 
 class OopsyGroup(click.Group):
@@ -24,6 +42,8 @@ class OopsyGroup(click.Group):
             click.echo('Invalid address ({}).'.format(e))
         except click.UsageError as e:
             click.echo('Error: {}'.format(e))
+        except click.ClickException as e:
+            click.echo('{}'.format(e))
 
 
 @click.group(name='oopsy', cls=OopsyGroup)
