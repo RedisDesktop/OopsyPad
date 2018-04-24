e1552fea217d79374085552ef9307fa021b264af
Igor Malinovskiy <u.glide@gmail.com>
2018-04-24 19:33:11 +0300

    Build & include binaries from bin folder

--- 3rdparty/build.sh
+++ 3rdparty/build.sh
@@ -17,3 +17,7 @@ function build_stackwalk {
 
 build_breakpad
 build_stackwalk
+
+cp $DIR/breakpad/src/processor/minidump_stackwalk $DIR/../oopsypad/bin/
+cp $DIR/breakpad/src/tools/linux/dump_syms/dump_syms $DIR/../oopsypad/bin/
+cp $DIR/minidump-stackwalk/stackwalker $DIR/../oopsypad/bin/
