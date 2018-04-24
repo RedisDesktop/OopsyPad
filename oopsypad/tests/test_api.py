adaa28fb6a20d54b5ad77f77f414bc4fffa0b276
ommrnom <ommarinaom@gmail.com>
2018-04-24 14:59:34 +0300

    Update tests

--- oopsypad/tests/test_api.py
+++ oopsypad/tests/test_api.py
@@ -1,17 +1,14 @@
 import base64
+import json
 import os
 import unittest
 from unittest.mock import patch
 
 from ddt import ddt, data, unpack
-from flask import url_for
-from flask_testing import LiveServerTestCase
+from flask_testing import TestCase
 import pymongo
-import requests
 
-from oopsypad.client.admin import save_token
-from oopsypad.client.symfile import send_symfile
-from oopsypad.client.minidump import send_crash_report
+from oopsypad.client.symfile import create_symfile
 from oopsypad.server import config, demo, models
 from oopsypad.server.app import create_app
 from oopsypad.tests.utils import fake_create_stacktrace_worker
@@ -22,7 +19,7 @@ LINUX = 'Linux'
 ALLOWED_PLATFORMS = [LINUX, 'MacOS', 'Windows']
 
 
-class TestBase(LiveServerTestCase):
+class TestBase(TestCase):
 
     def create_app(self):
         patch.object(models.Minidump, 'create_stacktrace',
@@ -53,37 +50,51 @@ class TestBase(LiveServerTestCase):
 
 @ddt
 class SymfileTest(TestBase):
+    url = '/api/data/symfile/{product}/{id}'
+
     @data((demo.ADMIN_TOKEN, 201),
           (demo.DEV_TOKEN, 403),
           (demo.SYM_TOKEN, 201))
     @unpack
     def test_send_symfile(self, token, status_code):
-        save_token(token)
-        response = send_symfile(
-            bin_path=os.path.join(config.Config.ROOT_DIR, 
-                                  'tests', 'integration', 'test_app', 'bin', 
-                                  'test_app'),
-            symfile_name='{}.sym'.format(TEST_APP),
-            address=self.get_server_url(),
-            version='0.9')
+        bin_path = os.path.join('tests', 'integration', 'test_app', 'bin',
+                                'test_app')
+        symfile_name = '{}.sym'.format(TEST_APP)
+        symfile_path = create_symfile(bin_path=bin_path,
+                                      symfile_name=symfile_name,
+                                      symfile_root=config.Config.SYMFILES_DIR)
+        with open(symfile_path, 'r') as f:
+            _, platform, _, id, product = f.readline().split()
+        with open(symfile_path, 'rb') as f:
+            headers = {'Authorization': base64.b64encode(token.encode())}
+            data = {'version': '0.9',
+                    'platform': platform,
+                    'symfile': f}
+            url = self.url.format(product=product, id=id)
+            response = self.client.post(url, data=data, headers=headers,
+                                        content_type='multipart/form-data')
+
         self.assertEqual(response.status_code, status_code)
         if token != demo.DEV_TOKEN:
-            print(response.json())
-            self.assertEqual(response.json(), {'ok': 'Symbol file was saved.'})
+            print(response.json)
+            self.assertEqual(response.json, {'ok': 'Symbol file was saved.'})
 
 
 @ddt
 class CrashReportTest(TestBase):
+    url = '/crash-report'
 
     def send_crash_report_response(self, product, version, platform):
 
-        minidump_path = os.path.join(config.Config.ROOT_DIR, 'oopsypad',
-                                     'tests', 'fixtures', 'minidump.dmp')
-        response = send_crash_report(address=self.get_server_url(),
-                                     dump_path=minidump_path,
-                                     product=product,
-                                     version=version,
-                                     platform=platform)
+        minidump_path = os.path.join('oopsypad', 'tests', 'fixtures',
+                                     'minidump.dmp')
+        with open(minidump_path, 'rb') as f:
+            data = {'product': product,
+                    'version': version,
+                    'platform': platform,
+                    'upload_file_minidump': f}
+            response = self.client.post(self.url, data=data,
+                                        content_type='multipart/form-data')
         return response
 
     @data(
@@ -99,28 +110,28 @@ class CrashReportTest(TestBase):
     def test_send_crash_report_validation(self, product, version, platform,
                                           fail_reason, error):
         response = self.send_crash_report_response(product, version, platform)
-        print(response.json())
+        print(response.json)
 
         self.assertEqual(response.status_code, 400,
                          'Minidump {} validation failed: {}'.format(
-                             fail_reason, response.json()))
-        self.assertEqual(response.json(), error, 'Wrong error.')
+                             fail_reason, response.json))
+        self.assertEqual(response.json, error, 'Wrong error.')
 
     def test_send_crash_report(self):
         product, version, platform = TEST_APP, '0.9', LINUX
         response = self.send_crash_report_response(product, version, platform)
-        print(response.json())
+        print(response.json)
 
         self.assertEqual(response.status_code, 201,
                          'Minidump send failed with {} code: {}'.format(
                              response.status_code,
-                             response.json()))
-        self.assertEqual(response.json(), {'ok': 'Thank you!'})
+                             response.json))
+        self.assertEqual(response.json, {'ok': 'Thank you!'})
 
     def test_minidump_content(self):
         product, version, platform = TEST_APP, '0.9', 'Windows'
         response = self.send_crash_report_response(product, version, platform)
-        print(response.json())
+        print(response.json)
 
         minidump = models.Minidump.objects(product=product).first()
         print(minidump)
@@ -136,66 +147,65 @@ class CrashReportTest(TestBase):
 
 @ddt
 class TokenTest(TestBase):
+    url = '/token'
+
     @data((demo.ADMIN_EMAIL, demo.ADMIN_TOKEN),
           (demo.DEV_EMAIL, demo.DEV_TOKEN),
           (demo.SYM_EMAIL, demo.SYM_TOKEN))
     @unpack
     def test_get_token(self, email, token):
-        url = '{}{}'.format(self.get_server_url(),
-                            url_for('oopsypad.get_auth_token'))
-        response = requests.get(url, auth=(email, demo.PSW))
+        headers = {'Authorization':  'Basic ' + str(base64.b64encode(
+            '{}:{}'.format(email, demo.PSW).encode()).decode())}
+        response = self.client.get(self.url, headers=headers)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json().get('token'), token)
+        self.assertEqual(response.json.get('token'), token)
 
 
 @ddt
 class ProjectsTest(TestBase):
+
     @data((demo.ADMIN_TOKEN, 201),
           (demo.DEV_TOKEN, 403),
           (demo.SYM_TOKEN, 403))
     @unpack
     def test_project_add(self, token, status_code):
-        headers = {'Authorization': base64.b64encode(token.encode())}
-        url = '{}{}'.format(self.get_server_url(),
-                            url_for('oopsypad_api.add_project', name=TEST_APP))
-        response = requests.post(url, headers=headers,
-                                 json={'min_version': MIN_VERSION,
-                                       'allowed_platforms': ALLOWED_PLATFORMS})
+        url = '/api/projects/{name}'.format(name=TEST_APP)
+        headers = {'content-type': 'application/json',
+                   'Authorization': base64.b64encode(token.encode())}
+        project_data = {'min_version': MIN_VERSION,
+                        'allowed_platforms': ALLOWED_PLATFORMS}
+        response = self.client.post(url, headers=headers,
+                                    data=json.dumps(project_data))
+
         self.assertEqual(response.status_code, status_code)
         if token == demo.ADMIN_TOKEN:
-            self.assertIsNotNone(response.json().get('ok'))
+            self.assertIsNotNone(response.json.get('ok'))
 
     @data((demo.ADMIN_TOKEN, 202),
           (demo.DEV_TOKEN, 403),
           (demo.SYM_TOKEN, 403))
     @unpack
     def test_project_delete(self, token, status_code):
+        url = '/api/projects/{name}/delete'.format(name=TEST_APP)
         headers = {'Authorization': base64.b64encode(token.encode())}
-        url = '{}{}'.format(self.get_server_url(),
-                            url_for('oopsypad_api.delete_project',
-                                    name=TEST_APP))
-        response = requests.delete(url, headers=headers,
-                                   json={'min_version': MIN_VERSION,
-                                         'allowed_platforms':
-                                             ALLOWED_PLATFORMS})
+        response = self.client.delete(url, headers=headers)
+
         self.assertEqual(response.status_code, status_code)
         if token == demo.ADMIN_TOKEN:
-            self.assertIsNotNone(response.json().get('ok'))
+            self.assertIsNotNone(response.json.get('ok'))
 
     @data((demo.ADMIN_TOKEN, 200),
           (demo.DEV_TOKEN, 403),
           (demo.SYM_TOKEN, 403))
     @unpack
     def test_project_list(self, token, status_code):
+        url = '/api/projects'
         headers = {'Authorization': base64.b64encode(token.encode())}
-        url = '{}{}'.format(self.get_server_url(),
-                            url_for('oopsypad_api.list_projects'))
-        response = requests.get(url, headers=headers,
-                                json={'min_version': MIN_VERSION,
-                                      'allowed_platforms': ALLOWED_PLATFORMS})
+        response = self.client.get(url, headers=headers)
+
         self.assertEqual(response.status_code, status_code)
         if token == demo.ADMIN_TOKEN:
-            self.assertIsNotNone(response.json().get('projects'))
+            self.assertIsNotNone(response.json.get('projects'))
 
 
 if __name__ == '__main__':
