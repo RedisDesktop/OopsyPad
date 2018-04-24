226a59a0d3a1ef169e81f5abfd90495041c744dd
ommrnom <ommarinaom@gmail.com>
2018-04-24 14:58:19 +0300

    Divide send symfile command into create and send

--- oopsypad/client/symfile.py
+++ oopsypad/client/symfile.py
@@ -27,31 +27,7 @@ def create_symfile(bin_path, symfile_name, symfile_root):
     return symfile_path
 
 
-@oopsy.command(name='oopsy_send_symfile')
-@click.argument('bin-path')
-@click.argument('symfile-name')
-@click.argument('version')
-def oopsy_send_symfile(bin_path, symfile_name, version):
-    """
-    \b
-    BIN-PATH
-        Product executable binary path.
-    SYMFILE-NAME
-        Target symbol file name.
-    VERSION
-        Product version.
-    """
-    response = send_symfile(bin_path, symfile_name, get_address(), version)
-    if response.status_code == 201:
-        click.echo(response.json().get('ok', 'OK'))
-    elif response.status_code == 403:
-        click.echo(response.reason.capitalize())
-    else:
-        click.echo(response.json().get('error', 'ERROR'))
-
-
-def send_symfile(bin_path, symfile_name, address, version):
-    symfile_path = create_symfile(bin_path, symfile_name, Config.SYMFILES_DIR)
+def send_symfile(symfile_path, address, version):
     with open(symfile_path, 'r') as f:
         _, platform, _, id, product = f.readline().split()
     with open(symfile_path, 'rb') as f:
@@ -62,3 +38,33 @@ def send_symfile(bin_path, symfile_name, address, version):
             '{}/api/data/symfile/{}/{}'.format(address, product, id),
             data=data, headers=headers, files=files)
     return r
+
+
+@oopsy.command(name='oopsy_create_symfile')
+@click.argument('bin-path')
+@click.argument('symfile-name')
+def oopsy_create_symfile(bin_path, symfile_name):
+    symfile_path = create_symfile(bin_path, symfile_name, Config.SYMFILES_DIR)
+    click.echo(symfile_path)
+
+
+@oopsy.command(name='oopsy_send_symfile')
+@click.argument('symfile-path')
+@click.argument('version')
+def oopsy_send_symfile(symfile_path, version):
+    """
+    \b
+    BIN-PATH
+        Product executable binary path.
+    SYMFILE-NAME
+        Target symbol file name.
+    VERSION
+        Product version.
+    """
+    response = send_symfile(symfile_path, get_address(), version)
+    if response.status_code == 201:
+        click.echo(response.json().get('ok', 'OK'))
+    elif response.status_code == 403:
+        click.echo(response.reason.capitalize())
+    else:
+        click.echo(response.json().get('error', 'ERROR'))
