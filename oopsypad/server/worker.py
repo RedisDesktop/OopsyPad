a7c41179450746ec6f9b1e0edb34bc4c4f8e7c5d
ommrnom <ommarinaom@gmail.com>
2018-04-27 15:33:51 +0300

    Add celery worker management commands

--- oopsypad/server/worker.py
+++ oopsypad/server/worker.py
@@ -1,11 +1,19 @@
 import logging
+import os
+import signal
+import subprocess
+from time import sleep
 
 from celery import Celery
+import click
 import raven
 from raven.contrib.celery import register_signal, register_logger_signal
 
 from oopsypad.server import models
 from oopsypad.server.run import app
+
+CELERY_LOG = os.path.join(app.config['ROOT_DIR'], 'celery.log')
+CELERYD_PID = os.path.join(app.config['ROOT_DIR'], 'celeryd.pid')
 
 
 def make_celery(app):
@@ -52,3 +60,48 @@ def process_minidump(minidump_id):
     minidump.get_stacktrace()
     minidump.parse_stacktrace()
     logger.info('Minidump {} was processed.'.format(minidump_id))
+
+
+@click.group('oopsy_celery_worker')
+def oopsy_celery_worker():
+    pass
+
+
+@oopsy_celery_worker.command('run')
+def oopsy_celery_worker_run():
+    if os.path.isfile(CELERYD_PID):
+        if click.confirm('Celery worker is already running. Restart?',
+                         default=True):
+            terminate_celery_worker()
+            click.echo('Restarting...')
+    while os.path.isfile(CELERYD_PID):
+        sleep(.25)
+    subprocess.run(['celery', 'worker', '-A', 'oopsypad.server.worker.celery',
+                    '--detach', '--loglevel', 'info', '--logfile', CELERY_LOG,
+                    '--pidfile', CELERYD_PID])
+    click.echo('Celery worker is running.')
+
+
+def terminate_celery_worker():
+    try:
+        with open(CELERYD_PID) as pid:
+            os.kill(int(pid.read()), signal.SIGTERM)
+            click.echo('Celery worker process was terminated.')
+    except OSError:
+        click.echo('Celery worker is not running.')
+
+
+@oopsy_celery_worker.command('stop')
+def oopsy_celery_worker_stop():
+    terminate_celery_worker()
+
+
+@oopsy_celery_worker.command('logs')
+@click.option('--number', '-n', default=0, type=int, help='Show last n entries.')
+def oopsy_celery_worker_logs(number):
+    try:
+        with open(CELERY_LOG) as logs:
+            for line in logs.readlines()[-number:]:
+                click.echo(line)
+    except OSError:
+        click.echo('No logfile found.')
