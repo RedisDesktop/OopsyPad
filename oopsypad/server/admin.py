12198fd677b27daf14f94e0caf68ac365429e2ce
ommrnom <ommarinaom@gmail.com>
2018-07-24 16:43:42 +0300

    Add symfile admin view

--- oopsypad/server/admin.py
+++ oopsypad/server/admin.py
@@ -1,18 +1,50 @@
 import calendar
 from datetime import datetime
+import os
 import random
 import warnings
 
 from dateutil.relativedelta import relativedelta
-from flask import current_app, flash, jsonify, redirect, request
+from flask import current_app, escape, flash, jsonify, Markup, redirect, request
 from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.base import MenuLink
-from flask_admin.contrib.mongoengine import ModelView
+from flask_admin.contrib.mongoengine import helpers, ModelView
+from flask_admin.contrib.mongoengine.view import DEFAULT_FORMATTERS
 from flask_admin.model.template import macro
 from flask_security import current_user
+from mongoengine import GridFSProxy
+from wtforms import StringField, validators
 
 from oopsypad.server import models
 from oopsypad.server.helpers import last_12_months
+
+
+def date_format(view, value):
+    return value.strftime('%d-%m-%Y %-I:%M %p')
+
+
+def grid_formatter(view, value):
+    if not value.grid_id:
+        return ''
+
+    args = helpers.make_gridfs_args(value)
+
+    return Markup(
+        '<a href="{url}" target="_blank" '
+        'title="{size}k ({content_type})">'
+        '<i class="fa fa-file glyphicon glyphicon-file"></i>'
+        '{name}</a> '.format(
+            url=view.get_url('.api_file_view', **args),
+            name=escape(value.name),
+            size=value.length // 1024,
+            content_type=escape(value.content_type)))
+
+
+CUSTOM_TYPE_FORMATTERS = DEFAULT_FORMATTERS
+CUSTOM_TYPE_FORMATTERS.update({
+    datetime: date_format,
+    GridFSProxy: grid_formatter
+})
 
 
 def get_random_color():
@@ -80,22 +112,23 @@ class DeveloperModelView(ModelView):
 
 
 class ProjectView(AdminModelView):
-    action_disallowed_list = ['delete']
+    can_delete = False
     can_view_details = True
     column_display_actions = False
     column_editable_list = ['name']
     column_formatters = dict(actions=macro('render_actions'))
-    column_list = ('name', 'actions')
+    column_labels = dict(min_version='Minimum Version')
+    column_list = ['name', 'min_version', 'allowed_platforms', 'actions']
     create_modal = True
     create_modal_template = 'admin/add_project_modal.html'
     create_template = 'admin/add_project.html'
     edit_template = 'admin/edit_project.html'
-    form_args = {
-        'min_version': {'label': 'Minimum required version of crashed app'},
-        'allowed_platforms': {'label': 'Allowed platforms'}
-    }
-    form_create_rules = {'name'}
+    form_args = dict(
+        min_version={'label': 'Minimum required version of crashed app'},
+        allowed_platforms={'label': 'Allowed platforms'})
+    form_create_rules = ('name',)
     form_edit_rules = ('min_version', 'allowed_platforms')
+    form_overrides = dict(min_version=StringField)
     list_template = 'admin/project_list.html'
 
     @expose('/details/')
@@ -141,29 +174,27 @@ class ProjectView(AdminModelView):
 
 class CrashReportView(DeveloperModelView):
     can_create = False
-    can_edit = False
     can_delete = False
+    can_edit = False
     column_display_actions = False
-    column_list = ('product', 'version', 'platform', 'date_created',
-                   'crash_reason')
     column_filters = ('product', 'version', 'platform', 'date_created',
                       'crash_reason')
+    column_list = ['product', 'version', 'platform', 'date_created',
+                   'crash_reason']
     list_template = 'admin/crash_report_list.html'
 
 
 class IssueView(DeveloperModelView):
     can_create = False
-    can_edit = False
     can_delete = False
+    can_edit = False
     can_view_details = True
     column_default_sort = ('total', True)
     column_display_actions = False
     column_filters = ('product', 'platform', 'reason')
     column_formatters = dict(actions=macro('render_actions'))
-    column_list = ('platform', 'version', 'reason', 'total', 'actions')
-    form_args = {
-        'total': {'label': 'Total Crash Reports'},
-    }
+    column_list = ['platform', 'version', 'reason', 'total', 'actions']
+    form_args = dict(total={'label': 'Total Crash Reports'})
     list_template = 'admin/issue_list.html'
     page_size = 10
 
@@ -185,15 +216,71 @@ class IssueView(DeveloperModelView):
 
 
 class UserView(AdminModelView):
-    column_list = ('email', 'active', 'roles')
+    column_list = ['email', 'active', 'roles']
+    form_edit_rules = ['email', 'roles', 'active']
+    form_overrides = dict(email=StringField)
+
+
+class SymfileView(AdminModelView):
+    can_edit = False
+    can_view_details = True
+    column_editable_list = ['version']
+    column_labels = dict(symfile_name='Filename',
+                         symfile_id='ID')
+    column_type_formatters = CUSTOM_TYPE_FORMATTERS
+    form_excluded_columns = ['product', 'platform', 'date_created',
+                             'symfile_id', 'symfile_name']
+    form_overrides = dict(version=StringField)
+
+    def on_model_change(self, form, model, is_created=None):
+        if hasattr(form, 'symfile') and form.symfile:
+            symfile = form.symfile.data
+            symfile.stream.seek(0)
+            try:
+                symfile_first_line = symfile.stream.readline().decode()
+                _, platform, _, symfile_id, product = symfile_first_line.split()
+            except Exception as e:
+                current_app.logger.exception(
+                    'Unable to parse symfile: {}'.format(e))
+                raise validators.ValidationError(
+                    'Unable to parse symfile invalid content.')
+
+            existing_symfile = models.Symfile.objects(
+                symfile_id=symfile_id).first()
+            if existing_symfile:
+                raise validators.ValidationError(
+                    'Symfile with this ID already exists.')
+            if is_created:
+                model.product = product
+                models.version = form.version.data
+                model.platform = platform
+                model.symfile_id = symfile_id
+                model.save_symfile(symfile)
+                symfile.close()
+
+    def on_model_delete(self, model):
+        if all(getattr(model, p)
+               for p in ['product', 'symfile_name', 'symfile_name']):
+
+            symfile = os.path.join(model.get_symfile_path(), model.symfile_name)
+            if os.path.isfile(symfile):
+                try:
+                    os.remove(symfile)
+                    current_app.logger.info(
+                        'Symfile {} for {} {} ({}) was deleted.'.format(
+                            symfile,
+                            model.product,
+                            model.version,
+                            model.platform))
+                except Exception as e:
+                    current_app.logger.exception(
+                        'Unable to delete symfile: {}'.format(e))
 
 
 admin = Admin(
     name='OopsyPad',
     template_mode='bootstrap3',
-    index_view=CustomAdminView(
-        template='index.html',
-        url='/')
+    index_view=CustomAdminView(template='index.html', url='/')
 )
 
 
@@ -206,7 +293,16 @@ with warnings.catch_warnings():
                             category=UserWarning)
     admin.add_view(ProjectView(models.Project, name='Projects'))
     admin.add_view(AdminModelView(models.Platform, name='Platforms'))
-    admin.add_view(CrashReportView(
-        models.Minidump, name='Crash Reports', endpoint='crash-reports'))
-    admin.add_view(IssueView(models.Issue, name='Issues'))
-    admin.add_view(UserView(models.User, name='Users'))
+    admin.add_view(SymfileView(models.Symfile, name='Symfiles',
+                               menu_icon_type='glyph',
+                               menu_icon_value='glyphicon-file'))
+    admin.add_view(CrashReportView(models.Minidump, name='Crash Reports',
+                                   endpoint='crash-reports',
+                                   menu_icon_type='glyph',
+                                   menu_icon_value='glyphicon-fire'))
+    admin.add_view(IssueView(models.Issue, name='Issues',
+                             menu_icon_type='glyph',
+                             menu_icon_value='glyphicon-flash'))
+    admin.add_view(UserView(models.User, name='Users',
+                            menu_icon_type='glyph',
+                            menu_icon_value='glyphicon-user'))
