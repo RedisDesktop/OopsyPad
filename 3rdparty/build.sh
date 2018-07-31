9a5350cf6d0e0587592de7aa1223c9f407cd661a
Igor Malinovskiy <u.glide@gmail.com>
2018-07-31 20:25:45 +0300

    Fix dump_syms compilation on OSX

--- 3rdparty/build.sh
+++ 3rdparty/build.sh
@@ -15,8 +15,6 @@ function build_breakpad {
     
     # NOTE(u_glide): Only client-mode is supported on OSX
     if [[ $OSTYPE == darwin* ]]; then
-        wget "https://02692989752462866653.googlegroups.com/attach/75865a958e446/dump_syms.patch?part=0.1&view=1&vt=ANaJVrE_51_bXadOKL5j07yqsuBjehkaBp20gX6b1WUQsRv4xpPJl02wUkUwypXr9JfpkUYrAgEnDzZ3rmLUdIj6Xryxb9kY4gL6kd0gVYxRSd-2rvAd-dE" -O dump_syms.patch
-        git apply dump_syms.patch
         xcodebuild -quiet -sdk macosx -project src/tools/mac/dump_syms/dump_syms.xcodeproj -configuration Release ARCHS=x86_64               
     else
         git clone https://chromium.googlesource.com/linux-syscall-support src/third_party/lss || true            
